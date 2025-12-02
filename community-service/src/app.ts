d13753f539d2404367473ee516a894ce1ad987c0
duongdinh2003 <lhdd1904@gmail.com>
2025-12-02 16:41:12 +0700

    fix

--- community-service/src/app.ts
+++ community-service/src/app.ts
@@ -1,23 +1,25 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import communityRoutes from '@/routes/community.route.js';
-import communityTypeRoutes from '@/routes/communityType.route.js';
-import communityMemberRoutes from '@/routes/communityMember.route.js'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import communityRoutes from "@/routes/community.route.js";
+import communityTypeRoutes from "@/routes/communityType.route.js";
+import communityMemberRoutes from "@/routes/communityMember.route.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGINS?.split(",") || ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use('/api/community-type', communityTypeRoutes);
-app.use('/api/community-member', communityMemberRoutes);
-app.use('/api/community', communityRoutes);
+app.use("/api/community-type", communityTypeRoutes);
+app.use("/api/community-member", communityMemberRoutes);
+app.use("/api/community", communityRoutes);
 
 export default app;
